ccf293058880de56c289d016634ffc87edbe461e
NlodeL <nlodelop@myuax.com>
2023-11-09 12:08:53 +0100

    me ayudo de proyectos anteriores y hago un intercambio de dos variables

--- main.cpp
+++ main.cpp
@@ -9,12 +9,26 @@ using namespace std;
 int suma(int a, int b) {
     return a + b;
 }
+// 3 Optimización de Memoria
+void intercambiar(char &c, char &d){
+    char temp = c;
+    c = d;
+    d = temp;
+    }
 
 int main() {
     // 1 Operaciones básicas
     cout << suma(6, 5) << endl;
+
     // 2 Gestión de estudiantes
     Estudiante estudiante1("Juan", 12, "2");
     estudiante1.mostrar_info();
 
+    // 3 Optimización de Memoria
+    char c = 'a';
+    char d = 'b';
+    printf("Antes del intercambio c = %c y d = %c \n" , c, d);
+    intercambiar(c, d);
+    cout << "Despues del intercambio: c = " << c << ", d = " << d << endl;
 }
+
